ca56f3b02d33fc2d23f41aaca9d0da43a1d1151e
Marian Marian <fenrir55@gmail.com>
2018-11-03 10:42:38 +0100

    skończony

---
+++ Linq3/EvaluatePrice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq3
+{
+    public class Purchase
+    {
+        public int Issue { get; set; }
+        public decimal Price { get; set; }
+
+        public static Dictionary<int, decimal> GetPrices()
+        {
+            return new Dictionary<int, decimal> {
+                { 6, 3600M },
+                { 19, 500M },
+                { 36, 650M },
+                { 57, 13525M },
+                { 68, 250M },
+                { 74, 75M },
+                { 83, 25.75M },
+                { 97, 35.25M },
+            };
+        }
+
+        public static PriceRange EvaluatePrice(decimal price)
+        {
+            if (price < 100M)
+                return PriceRange.Cheap;
+            else if (price <= 1000M)
+                return PriceRange.MidRange;
+            else
+                return PriceRange.Expensive;
+             
+        }
+    }
+}
--- Linq3/Program.cs
+++ Linq3/Program.cs
@@ -8,8 +8,40 @@ namespace Linq3
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            Dictionary<int, decimal> values = Purchase.GetPrices();
+            var priceGroups =
+             from pair in values
+             group pair.Key by Purchase.EvaluatePrice(pair.Value)
+             into priceGroup
+             orderby priceGroup.Key descending
+             select priceGroup;
+
+            foreach (var group in priceGroups)
+            {
+                string stringKey;
+                switch (group.Key)
+                {
+                    case PriceRange.Cheap:
+                        stringKey = "tanie";
+                        break;
+                    case PriceRange.MidRange:
+                        stringKey = "średnie";
+                        break;
+                    default:
+                        stringKey = "tanie";
+                        break;
+                }
+                Console.Write($"Znalazłem {group.Count()} {stringKey} ");
+                foreach (var issue in group)
+                {
+                    Console.Write(issue.ToString() + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
         }
     }
 }
